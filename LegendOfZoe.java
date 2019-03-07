8dea7b058e323cb873e80e703d18367c1e3cb2a7
Rui Ze Ma <mmrz33@gmail.com>
2019-03-06 23:02:02 -0500

    Initial commit

---
+++ LegendOfZoe.java
@@ -0,0 +1,11 @@
+/**
+ * Classe principale du programme.
+ *
+ * NOTEZ : VOUS NE DEVEZ PAS RENOMMER CETTE CLASSE
+ */
+public class LegendOfZoe {
+
+    public static void main(String[] args) {
+        // TODO : Votre code de fonction main ici
+    }
+}
